e47f77b089dad3cb62fa6bafb6a849ea14b93839
Mike Enochs <msenochs@umich.edu>
2019-10-16 21:30:15 -0400

    Pushing build directory to use Netlify

---
+++ build/precache-manifest.166e910f00332efa0a52a1d6cbfa6b28.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e525180e0d2fbcb60021ba6c223aa94",
+    "url": "/index.html"
+  },
+  {
+    "revision": "113c2d3d7798fb0e2eb8",
+    "url": "/static/js/2.5cc80f79.chunk.js"
+  },
+  {
+    "revision": "c9e7b3906e021219c716",
+    "url": "/static/js/main.4bafa83f.chunk.js"
+  },
+  {
+    "revision": "88c2a2f72a85c1bb4baa",
+    "url": "/static/js/runtime-main.7a5721d6.js"
+  }
+]);
